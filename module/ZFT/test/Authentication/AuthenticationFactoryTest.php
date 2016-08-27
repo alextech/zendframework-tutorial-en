fc39a5ccfc6af333821d2ed331614b26d0bf5e9e
Sasha Alex Romanenko <alex@alex-tech-adventures.com>
2016-08-27 00:20:31 -0500

    Establish expectation for authentication data flow from object creation to in memory session identity member.

--- module/ZFT/test/Authentication/AuthenticationFactoryTest.php
+++ module/ZFT/test/Authentication/AuthenticationFactoryTest.php
@@ -2,10 +2,11 @@
 namespace ZFTest\Authentication;
 
 
+use Zend\Authentication;
 use Zend\Authentication\Adapter\AdapterInterface;
 use Zend\Authentication\AuthenticationService;
-use Zend\Authentication\Storage\StorageInterface;
 use Zend\ServiceManager\ServiceManager;
+use Zend\Session;
 use ZFT\Authentication\AuthenticationServiceFactory;
 
 class AuthenticationFactoryTest extends \PHPUnit_Framework_TestCase {
@@ -18,6 +19,7 @@ class AuthenticationFactoryTest extends \PHPUnit_Framework_TestCase {
 
         $this->sm = new ServiceManager();
         $this->sm->setService('Configuration', require __DIR__.'/../../../../config/autoload/global.php');
+        $this->sm->setService('authentication', new AuthenticationServiceFactory());
     }
 
     public function testCanCreateAuthenticationService() {
@@ -30,6 +32,29 @@ class AuthenticationFactoryTest extends \PHPUnit_Framework_TestCase {
         $this->assertInstanceOf(AdapterInterface::class, $authService->getAdapter());
 
         $rs = $authService->authenticate();
+
+        return;
+    }
+
+    public function testIdentityCreated() {
+        /** @var AuthenticationServiceFactory $authServiceFactory */
+        $authServiceFactory = $this->sm->get('authentication');
+
+        /** @var AuthenticationService $auth */
+        $auth = $authServiceFactory($this->sm, 'authentication');
+        $auth->clearIdentity();
+
+        /** @var Authentication\Adapter\Ldap $adapter */
+        $adapter = $auth->getAdapter();
+        $adapter->setIdentity('zftutorial');
+        $adapter->setPassword('ZFT2016!');
+
+        $auth->authenticate();
+
+        $container = new Session\Container(Authentication\Storage\Session::NAMESPACE_DEFAULT);
+        $identity = $container->{Authentication\Storage\Session::MEMBER_DEFAULT};
+
+        $this->assertEquals('alex-tech\\zftutorial', $identity);
 
         return;
     }
