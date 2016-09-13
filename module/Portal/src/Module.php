c6b3c692be7c3c7207e897713f21ef25d13d9226
Sasha Alex Romanenko <alex@alex-tech-adventures.com>
2016-09-13 01:40:23 -0400

    Trigger Mvc Error event on incomplete database. Catch error and direct to migrations tool.

--- module/Portal/src/Module.php
+++ module/Portal/src/Module.php
@@ -7,10 +7,13 @@
 
 namespace Portal;
 
+use Portal\Controller\AdminController;
 use Zend\Authentication\AuthenticationService;
 use Zend\EventManager\EventInterface;
+use Zend\Http\PhpEnvironment\Response;
 use Zend\ModuleManager\Feature\BootstrapListenerInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\Mvc\Application;
 use Zend\Mvc\MvcEvent;
 
 class Module implements ConfigProviderInterface, BootstrapListenerInterface {
@@ -33,6 +36,24 @@ class Module implements ConfigProviderInterface, BootstrapListenerInterface {
 
             return;
         }, 100);
+
+        $application->getEventManager()->attach(MvcEvent::EVENT_DISPATCH_ERROR, function(MvcEvent $e) {
+            $params = $e->getParams();
+
+            // @TODO better error handling
+            if(! (array_key_exists('needsDatabaseUpdate', $params) && $params['needsDatabaseUpdate'] === true)) return;
+
+            $router = $e->getRouter();
+            $url = $router->assemble([], ['name' => 'admin/dbtools']);
+
+            /** @var Response $response */
+            $response = $e->getResponse();
+            $response->setStatusCode(302);
+            $response->getHeaders()->addHeaderLine('Location', $url);
+            $response->sendHeaders();
+
+            return $response;
+        });
 
         return;
     }
--- module/ZFT/Module.php
+++ module/ZFT/Module.php
@@ -3,6 +3,7 @@
 namespace ZFT;
 
 use Zend\Db\Adapter\AdapterInterface;
+use Zend\EventManager\Event;
 use Zend\ModuleManager\Feature\ServiceProviderInterface;
 use Zend\Mvc\MvcEvent;
 use Zend\ServiceManager\Factory\InvokableFactory;
@@ -20,13 +21,23 @@ class Module implements ServiceProviderInterface {
         $application = $e->getApplication();
         $sm = $application->getServiceManager();
 
-        $application->getEventManager()->attach(MvcEvent::EVENT_DISPATCH, function(MvcEvent $e) use ($sm) {
+        $em = $application->getEventManager();
+        $em->attach(MvcEvent::EVENT_DISPATCH, function(MvcEvent $e) use ($sm, $em) {
             $router = $e->getRouteMatch();
             if(!($router->getParam('needsDatabase') === false)) {
                 $adapter = $sm->get('dbcon');
 
                 $migrations = new Migrations($adapter);
-                $migrations->needsUpdate();
+                if($migrations->needsUpdate()) {
+                    $e->setName(MvcEvent::EVENT_DISPATCH_ERROR);
+                    $e->setError('Database Needs Update');
+                    $e->setParam('needsDatabaseUpdate', true);
+
+                    $e->stopPropagation(true);
+                    $em->triggerEvent($e);
+
+                    return $e->getResult();
+                }
             }
         }, 100);
     }
