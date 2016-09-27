cf2afff7eac2a5694f0ac09795ce320592b0026f
Sasha Alex Romanenko <alex@alex-tech-adventures.com>
2016-09-27 07:49:32 -0400

    flashmessenger routing recording prep

--- module/Portal/src/Controller/ProfileController.php
+++ module/Portal/src/Controller/ProfileController.php
@@ -7,10 +7,14 @@ use Zend\Mvc\Controller\AbstractActionController;
 class ProfileController extends AbstractActionController {
 
     public function viewAction() {
-
+        // view profile
     }
 
     public function editAction() {
+        // edit form
+    }
 
+    public function submitAction() {
+        // database insert/update
     }
 }
