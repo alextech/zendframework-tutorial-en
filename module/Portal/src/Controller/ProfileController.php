e28188d4835ea0a14e9b94bb73095e2a40218811
Sasha Alex Romanenko <alex@alex-tech-adventures.com>
2016-09-28 01:00:01 -0400

    #2 add messages to Flash Messenger basec on success of submit action

--- module/Portal/src/Controller/ProfileController.php
+++ module/Portal/src/Controller/ProfileController.php
@@ -3,6 +3,7 @@
 namespace Portal\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Mvc\Plugin\FlashMessenger\FlashMessenger;
 
 class ProfileController extends AbstractActionController {
 
@@ -19,11 +20,16 @@ class ProfileController extends AbstractActionController {
 
         // flashmessage
 
+        /** @var FlashMessenger $flashMessenger */
+        $flashMessenger = $this->flashMessenger();
         $success = true;
         if($success) {
             $destinationRoute = 'profile/view_profile';
+            $flashMessenger->addSuccessMessage('Profile successfully saved');
         } else {
             $destinationRoute = 'profile/edit_profile/form_profile';
+            $flashMessenger->addErrorMessage('Invalid email');
+            $flashMessenger->addErrorMessage('Invalid username');
         }
 
         return $this->redirect()->toRoute($destinationRoute);
